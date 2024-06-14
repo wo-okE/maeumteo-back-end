f659cc87cadfeae8f06eb4c3ff8019c2dd72b320
wo-okE <seugn97@gmail.com>
2024-06-14 11:33:50 +0900

    프로젝트 생성

---
+++ src/main/java/com/maeumteo/maeumteobackend/MaeumteoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.maeumteo.maeumteobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeumteoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaeumteoBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maeumteo/maeumteobackend/MaeumteoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maeumteo.maeumteobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaeumteoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
